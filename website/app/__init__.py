349fb96e176e09fc25fd484e196ac11d92c40f7c
Mohamed Adham Mahrous <s-mohamed.adham@zewailcity.edu.eg>
2021-03-15 20:18:41 +0200

    Set launch option and black as formatter (#5)
    * Set launch option and black as formatter
    
    * format-code

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -21,8 +21,8 @@ compress = Compress()
 
 # Set up Flask-Login
 login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'account.login'
+login_manager.session_protection = "strong"
+login_manager.login_view = "account.login"
 
 
 def create_app(config):
@@ -30,10 +30,10 @@ def create_app(config):
     config_name = config
 
     if not isinstance(config, str):
-        config_name = os.getenv('FLASK_CONFIG', 'default')
+        config_name = os.getenv("FLASK_CONFIG", "default")
 
     app.config.from_object(Config[config_name])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     # not using sqlalchemy event system, hence disabling it
 
     Config[config_name].init_app(app)
@@ -48,33 +48,38 @@ def create_app(config):
 
     # Register Jinja template functions
     from .utils import register_template_utils
+
     register_template_utils(app)
 
     # Set up asset pipeline
     assets_env = Environment(app)
-    dirs = ['assets/styles', 'assets/scripts']
+    dirs = ["assets/styles", "assets/scripts"]
     for path in dirs:
         assets_env.append_path(os.path.join(basedir, path))
     assets_env.url_expire = True
 
-    assets_env.register('app_css', app_css)
-    assets_env.register('app_js', app_js)
-    assets_env.register('vendor_css', vendor_css)
-    assets_env.register('vendor_js', vendor_js)
+    assets_env.register("app_css", app_css)
+    assets_env.register("app_js", app_js)
+    assets_env.register("vendor_css", vendor_css)
+    assets_env.register("vendor_js", vendor_js)
 
     # Configure SSL if platform supports it
-    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+    if not app.debug and not app.testing and not app.config["SSL_DISABLE"]:
         from flask_sslify import SSLify
+
         SSLify(app)
 
     # Create app blueprints
     from .main import main as main_blueprint
+
     app.register_blueprint(main_blueprint)
 
     from .account import account as account_blueprint
-    app.register_blueprint(account_blueprint, url_prefix='/account')
+
+    app.register_blueprint(account_blueprint, url_prefix="/account")
 
     from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    app.register_blueprint(admin_blueprint, url_prefix="/admin")
 
     return app
