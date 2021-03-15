cacf59f334e3a58c6430ef23feae43491e1309a7
Mohamed Mahrous <s-mohamed.adham@zewailcity.edu.eg>
2021-03-15 20:11:00 +0200

    Set launch option and black as formatter

--- website/manage.py
+++ website/manage.py
@@ -11,7 +11,7 @@ from app import create_app, db
 from app.models import Role, User
 from config import Config
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv("FLASK_CONFIG") or "default")
 manager = Manager(app)
 migrate = Migrate(app, db)
 
@@ -20,9 +20,9 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
 
-manager.add_command('shell', Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
-manager.add_command('runserver', Server(host="0.0.0.0"))
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
+manager.add_command("runserver", Server(host="0.0.0.0", port=8080))
 
 
 @manager.command
@@ -30,7 +30,7 @@ def test():
     """Run the unit tests."""
     import unittest
 
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
@@ -46,12 +46,13 @@ def recreate_db():
 
 
 @manager.option(
-    '-n',
-    '--number-users',
+    "-n",
+    "--number-users",
     default=10,
     type=int,
-    help='Number of each model type to create',
-    dest='number_users')
+    help="Number of each model type to create",
+    dest="number_users",
+)
 def add_fake_data(number_users):
     """
     Adds fake data to the database.
@@ -73,31 +74,33 @@ def setup_prod():
 
 def setup_general():
     """Runs the set-up needed for both local development and production.
-       Also sets up first admin user."""
+    Also sets up first admin user."""
     Role.insert_roles()
-    admin_query = Role.query.filter_by(name='Administrator')
+    admin_query = Role.query.filter_by(name="Administrator")
     if admin_query.first() is not None:
         if User.query.filter_by(email=Config.ADMIN_EMAIL).first() is None:
             user = User(
-                first_name='Admin',
-                last_name='Account',
+                first_name="Admin",
+                last_name="Account",
                 password=Config.ADMIN_PASSWORD,
                 confirmed=True,
-                email=Config.ADMIN_EMAIL)
+                email=Config.ADMIN_EMAIL,
+            )
             db.session.add(user)
             db.session.commit()
-            print('Added administrator {}'.format(user.full_name()))
+            print("Added administrator {}".format(user.full_name()))
 
 
 @manager.command
 def run_worker():
     """Initializes a slim rq task queue."""
-    listen = ['default']
+    listen = ["default"]
     conn = Redis(
-        host=app.config['RQ_DEFAULT_HOST'],
-        port=app.config['RQ_DEFAULT_PORT'],
+        host=app.config["RQ_DEFAULT_HOST"],
+        port=app.config["RQ_DEFAULT_PORT"],
         db=0,
-        password=app.config['RQ_DEFAULT_PASSWORD'])
+        password=app.config["RQ_DEFAULT_PASSWORD"],
+    )
 
     with Connection(conn):
         worker = Worker(map(Queue, listen))
@@ -107,15 +110,15 @@ def run_worker():
 @manager.command
 def format():
     """Runs the yapf and isort formatters over the project."""
-    isort = 'isort -rc *.py app/'
-    yapf = 'yapf -r -i *.py app/'
+    isort = "isort -rc *.py app/"
+    yapf = "yapf -r -i *.py app/"
 
-    print('Running {}'.format(isort))
+    print("Running {}".format(isort))
     subprocess.call(isort, shell=True)
 
-    print('Running {}'.format(yapf))
+    print("Running {}".format(yapf))
     subprocess.call(yapf, shell=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager.run()
